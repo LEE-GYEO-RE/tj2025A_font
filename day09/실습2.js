68cb0d55c791733433098b0b476ac59ffc44ff21
LEE-GYEO-RE <rufp9891@gmail.com>
2025-06-09 16:54:32 +0900

    실습2

---
+++ day09/실습2.js
@@ -0,0 +1,36 @@
+// 문제 1번
+console.log("[주문 접수]")
+
+let id=prompt("손님의 이름")
+let drink=prompt("원하는 음료")
+let number=prompt("주문 수량")
+
+console.log("이름")
+console.log(id)
+
+console.log("메뉴")
+console.log(drink)
+
+console.log("수량")
+console.log(number)
+
+// 문제 2번
+console.log("[운동 기록]")
+
+let id2=prompt("운동한 사람이름")
+let type=prompt("운동한 종류")
+let time=prompt("총 운동시간")
+
+console.log("이름")
+console.log(id2)
+
+console.log("운동종류")
+console.log(type)
+
+console.log("운동시간")
+console.log(time)
+
+console.log("칼로리")
+let area = time*7
+console.log(area)
+
