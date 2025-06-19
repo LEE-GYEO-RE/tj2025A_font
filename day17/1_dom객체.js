27eaf526cb7a3c0cd190630ef5cf15e1814b6b3d
LEE-GYEO-RE <rufp9891@gmail.com>
2025-06-19 11:35:05 +0900

    dom객체

--- day17/1_dom객체.js
+++ day17/1_dom객체.js
@@ -16,9 +16,14 @@
             (1) document.write('출력할내용' )       : HTML에 문자열 출력하는 함수    
             (2) document.querySelector('선택자')    : HTML에 지정한 선택자를 JS객체로 *반환하는 함수
             (3) document.querySelectorAll           : HTML에 지정한 선택자를 모두 JS객체(배열)로 *반환하는 함수
-                *선택자 : 1.마크업명 2. .클래스명 3. #id명
+                * 선택자 : 1.마크업명 2. .클래스명 3. #id명
             (4) dom객체.innerHTML                   : 마크업내 html내용물을 호출 하는 속성
-                *<마크업>--- 여기가 innerHTML ---</마크업>
+                * <마크업>--- 여기!!!! innerHTML ---</마크업>
+                * inner 없음 : <input /> , <img /> --> 혼자 닫는 애들 못 씀
+            (5) dom객체.value
+                * <마크업 value = "여기!!!"/>
+                * value 있음 : <input> , <select> , <textarea> , <radio> 등등
+                * value 없음 : <div> , <span> , <table> 등등
 
 */
 // [1]
@@ -39,3 +44,28 @@ console.log( divArray);
 const div2 = document.querySelector('div');
 const html = div2.innerHTML;
 console.log(html);      // <div> 박스1 </div> --> 박스1
+// [6]
+// 함수 실행 조건 : [함수1실행] 버튼을 클릭 했을 때
+// 매개변수 : x , 반환값 : x , 
+// 로직 : 아래 참고
+function func1(){   // 함수 선언
+    // 1. 함수 실행 테스트 출력
+    console.log('--- func1 exe ---')
+    // 2. input 마크업을 JS객체를 가져오기
+    const myInput = document.querySelector('.myInput')
+        console.log( myInput);  // 확인
+    // 3. 가져온 JS dom객체내 value 속성값 가져오기
+    const text = myInput.value;
+        console.log( text );
+    // vs
+        // document.querySelector('.myInput').value;
+} // func end
+// [7]
+function func2(){
+    console.log('--- func2 exe ---')
+
+    document.querySelector('.title').innerHTML = 'JS에서 작성한것';
+        // vs
+    // const title = document.querySelector('.title');
+    // title.innerHTML = 'JS에서 작성한 것';
+}
