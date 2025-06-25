08559166b4293033daadf6089d217fcb01463cb0
LEE-GYEO-RE <rufp9891@gmail.com>
2025-06-25 13:18:36 +0900

    0625 오전_cdn

--- day21/1_interval.js
+++ day21/1_interval.js
@@ -5,7 +5,12 @@
             (1) setInterval ( 함수명 , 밀리초 )
                 - 함수명 : 함수명만 작성 , ( ) 생략하여 실행하지 않는다.
                 - 밀리초 : 1/1000초 , 1000 : 1초 , 10000 : 10초 , 100 : 0.1초
+                === 밀리초마다 함수 실행
+                    -> 반환값 ( 함수실행 후 , 반환되는 결과 )
+                    let 변수 = setInterval( );
 
+            (2) clearInterval ( interval 객체 ); 
+                - 중지할 interval 객체를 대입하여 중지하는 함수 
 */
 
 // [1] 예제1
@@ -24,3 +29,47 @@ function 증가함수(){ // value 1 증가 함수 선언
 
 // ========== ** 간격(1초) 에 따른 '증가함수' 재실행 ** =========== //
 setInterval( 증가함수 , 1000 );     // 1000밀리초(1초) 간격으로 '증가함수' 실행
+
+// [2] 예제2
+function 시계함수(){
+    // 1. 현재 시간 구하기 : new Date() 객체
+    let today = new Date();
+    let hour = today.getHours();        // '시' 반환
+    let minute = today.getMinutes();    // '분' 반환
+    let second = today.getSeconds();    // '초' 반환
+    // 2. 특정한 위치에 값 대입
+        // (1) 어디에
+        const box2 = document.querySelector('#box2');
+        // (2) 무엇을
+        let html = `${hour} : ${minute} : ${second}`; // `백틱주의` 
+        // (3) 대입
+        box2.innerHTML = html;
+
+} // func end
+// *** 1초마다 시계함수 호출하기 ***
+setInterval(시계함수 , 1000 );
+
+// [3] 예제3
+
+let time = 0 ;  // 현재 타이머의 초
+let timerId;    // 변수만 선언 , 추후에 interval 객체저장
+
+// (1) 
+function 시간증가(){
+    time++                                          // 1증가
+    document.querySelector('#box3').innerHTML = time; // 출력  .value = input 박스 같은거 씀
+} // func end
+
+// (2)
+function 타이머시작(){
+    // 전역변수에 interval 객체 저장.
+    // 전역변수 사용이유 : 서로 다른 함수에서 공통 변수를 사용이 가능하다.
+    // vs 지역변수 : 함수 안에서만 사용한다.
+    timerId = setInterval( 시간증가 , 1000 );                 // 1초마다 '시간증가' 함수 실행
+    // 방법1 : return;
+} // func end
+
+// (3) 
+function 타이머중지(){
+    clearInterval( timerId );
+} // func end
